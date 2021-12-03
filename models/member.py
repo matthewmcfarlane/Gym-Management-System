ab5a62e2f8842e779f5da60fb72b9cc8434bb097
Matthew <matthewmcfarlane1@outlook.com>
2021-12-03 19:04:30 +0000

    added email address to Member class

--- models/member.py
+++ models/member.py
@@ -1,9 +1,10 @@
 class Member:
     def __init__(
-        self, first_name, last_name, dob=None, membership_type="PPV", active=False, id=None
+        self, first_name, last_name, email =None, dob=None, membership_type="PPV", active=False, id=None
     ):
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email
         self.dob = dob
         self.membership_type = membership_type
         self.active = active
