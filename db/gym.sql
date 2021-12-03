52d71457241e67d5bf6c2f2207705b205051a456
Matthew <matthewmcfarlane1@outlook.com>
2021-12-03 19:03:24 +0000

    created sql tables

---
+++ db/gym.sql
@@ -0,0 +1,29 @@
+DROP TABLE bookings;
+DROP TABLE members;
+DROP TABLE fitness_classes;
+
+CREATE TABLE members(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR (255),
+    dob VARCHAR (255),
+    email VARCHAR(255),
+    membership_type VARCHAR(255),
+    active BOOLEAN
+);
+
+CREATE TABLE fitness_classes(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    active BOOLEAN,
+    capacity INT
+    start_time VARCHAR(255)
+    duration INT,
+    day VARCHAR(255)
+    );
+
+CREATE TABLE bookings(
+    id SERIAL PRIMARY KEY,
+    fitness_class_key INT,
+    member_key INT
+);
