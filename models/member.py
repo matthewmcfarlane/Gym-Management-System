341af83d2ec9d805eb97ba094656813448b664be
Matthew <matthewmcfarlane1@outlook.com>
2021-12-03 12:33:26 +0000

    added id to member class

---
+++ models/member.py
@@ -0,0 +1,12 @@
+class Member:
+    def __init__(
+        self, first_name, last_name, dob=None, membership_type="PPV", active=False, id=None
+    ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.membership_type = membership_type
+        self.active = active
+        self.id = id
+        self.memberships = ["Gold", "Silver", "Bronze", "PPV"]
+
--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -1,10 +1,1 @@
-class Member:
-    def __init__(
-        self, first_name, last_name, dob=None, membership_type="PPV", active=False
-    ):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.dob = dob
-        self.membership_type = membership_type
-        self.active = active
-        self.memberships = ["Gold", "Silver", "Bronze", "PPV"]
+
