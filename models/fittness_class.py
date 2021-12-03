f65178aa4841fd47e9690b161dc78365290bfdec
Matthew <matthewmcfarlane1@outlook.com>
2021-12-03 14:05:39 +0000

    bug fix in fitness_class.py

--- models/fittness_class.py
+++ models/fittness_class.py
@@ -1,9 +1,9 @@
 class FitnessClass:
-    def __init__(self, name, capacity, active = False, start_time, duration, day, id=None):
+    def __init__(self, name, capacity, start_time, duration, day, active = False, id=None):
         self.name = name
         self.capacity = capacity
-        self.active = active
         self.start_time = start_time
         self.duration = duration
         self.day = day
+        self.active = active
         self.id = id
