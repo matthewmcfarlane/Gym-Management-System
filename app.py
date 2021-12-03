5984fb8b4b84ed05fe9a5cfc0313240758a9c738
Matthew <matthewmcfarlane1@outlook.com>
2021-12-03 23:55:10 +0000

    css and base styling

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', current_page = 'Home')
 
 if __name__ == '__main__':
     app.run(debug=True)
