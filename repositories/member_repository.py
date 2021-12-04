84fb8a6462685482a552d47a0412362d0a816d34
Matthew <matthewmcfarlane1@outlook.com>
2021-12-04 20:22:26 +0000

    added update member

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -34,3 +34,8 @@ def delete(id):
     sql = "DELETE FROM members WHERE id = %s"
     values = [id]
     run_sql(sql, values)
+
+def update(member):
+    sql = "UPDATE members SET (first_name, last_name, dob, email, membership_type, active) = (%s, %s, %s, %s, %s, %s) WHERE id = %s "
+    values = [member.first_name, member.last_name, member.dob, member.email, member.membership_type, member.active, member.id]
+    run_sql(sql, values)
