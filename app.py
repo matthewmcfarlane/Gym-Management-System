c49b9b1a54b5bc8ea5d3f9ec53ffd5f887fb3211
Matthew <matthewmcfarlane1@outlook.com>
2021-12-04 20:18:00 +0000

    added /members to controller

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
-
+from controllers.member_controller import members_blueprint
 app = Flask(__name__)
+
+# app.register_blueprint(bookings_blueprint)
+# app.register_blueprint(classes_blueprint)
+app.register_blueprint(members_blueprint)
 
 @app.route('/')
 def home():
--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, redirect, request, Blueprint
 
+from models.member import Member
+import repositories. member_repository as member_repository
+
+
+members_blueprint = Blueprint("members", __name__)
+
+@members_blueprint.route("/members")
+def members():
+    members = member_repository.select_all()
+    return render_template("members/index.html", members = members, current_page = 'Members')
 
 
